a5e632815f9a20a478cfe553c7c5993d1cdbb36a
Vermouth5214 <oeidonny.winarto@gmail.com>
2020-06-05 12:41:43 +0700

    nambahi keterangan aaa cirebon

--- laravel_redeem/resources/views/backend/redeem/view.blade.php
+++ laravel_redeem/resources/views/backend/redeem/view.blade.php
@@ -211,7 +211,7 @@
                             </table>
                             <?php
                                 // GROUP AAA ada kolom keterangan
-                                if (($userinfo['reldag'] == '14F01020002') || ($userinfo['reldag'] == '14B02010003') || ($userinfo['reldag'] == '22A02010002') || ($userinfo['reldag'] == '14J01010002')) :
+                                if (($userinfo['reldag'] == '14F01020002') || ($userinfo['reldag'] == '14B02010003') || ($userinfo['reldag'] == '22A02010002') || ($userinfo['reldag'] == '14J01010002') || ($userinfo['reldag'] == '13F01010003')) :
                             ?>
                             <div class="row">
                                 <div class="col-xs-12 col-sm-6">
